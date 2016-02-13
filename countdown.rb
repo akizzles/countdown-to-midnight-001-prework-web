4daf4c5bcd57412ea3c93762518e315b651b9812
akizzles <aykim002@gmail.com>
2016-02-12 22:57:51 -0500

    countdown exercise

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  def countdown_with_sleep(secs)
+    sleep(secs)
+  end
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    countdown_with_sleep(1)
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
